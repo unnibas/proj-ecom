463b568372b57e30c1c6e4d2d53e480f62bbdd25
unnibas <unnibas@gmail.com>
2021-05-09 19:27:36 +0530

    created custom button component and custom input component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,8 @@ const Header = () => (
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/shop'>CONTACT</Link>
+            <Link className='option' to='#'>CONTACT</Link>
+            <Link className='option' to='/signin'>SIGN IN</Link>
         </div>
     </div>
 )
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event =>{
+        event.preventDefault();
+        this.setState({ email: '', password: ''} );
+    }
+
+    handleChange = event =>{
+        const { value, name } = event.target;
+        this.setState({[name]: value});
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name='email' label='Email' value={this.state.email} type='email' required onChange={this.handleChange} />
+                    <FormInput name='password' label='Password' value={this.state.password} type='password' required onChange={this.handleChange} />
+                    <CustomButton type='submit'>SIGN IN</CustomButton>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default SignIn;
